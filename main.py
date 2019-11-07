d3ee60ab945ff848ec293359a3576880517f2a23
frankc95 <blazewicz.j@gmail.com>
2019-11-07 19:52:23 +0000

    added more templates

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template      
+import ConfigParser
+
+from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '\xe3\x03\xc7\xa9\x9a\xd7\xfe\xf7\x12\xe1\xc2##D\xa4R\xcf\x9e\xb3\x96}s\xd8\x99'
+
+app.secret_key = '\xe3\x03\xc7\xa9\x9a\xd7\xfe\xf7\x12\xe1\xc2##D\xa4R\xcf\x9e\xb3\x96}s\xd8\x99'
 
 posts = [
     {
@@ -37,5 +45,43 @@ def resources():
 def review():
     return render_template('review.html', title='Reviews', posts=posts)
 
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/config/')
+def config():
+    str = []
+    str.append('Debug:'+app.config['DEBUG'])
+    str.append('port:'+app.config['port'])
+    str.append('url:'+app.config['url'])
+    str.append('ip_address:'+app.config['ip_address'])
+    return '\t'.join(str)
+
+def init(app):
+    config = ConfigParser.ConfigParser()
+    try:
+        config_location = "etc/defaults.cfg"
+        config.read(config_location)
+
+        app.config['DEBUG'] = config.get("config", "debug")
+        app.config['ip_address'] = config.get("config", "ip_address")
+        app.config['port'] = config.get("config", "port")
+        app.config['url'] = config.get("config", "url")
+    except:
+        print "Could not read configs from:, config_location"
+
+        init(app)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    init(app)
+    app.run(
+            host=app.config['ip_address'],
+            port=int(app.config['port']))
