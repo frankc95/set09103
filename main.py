4c49d4621054029a0bff3aa033edd91b56b4cbf5
frankc95 <blazewicz.j@gmail.com>
2019-11-08 00:01:32 +0000

    updated logging and register forms

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import ConfigParser
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -45,14 +45,23 @@ def resources():
 def review():
     return render_template('review.html', title='Reviews', posts=posts)
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created for {}!'.format(form.username.data), 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check your username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
