8386d07403f71dcf2cb5fc7544e5a597a30a3d2a
frankc95 <blazewicz.j@gmail.com>
2019-11-12 21:15:49 +0000

    added update and delete post

--- bop/routes.py
+++ bop/routes.py
@@ -1,31 +1,17 @@
 import os
 import secrets
 from PIL import Image
-from flask import  render_template, flash, redirect, url_for, request
+from flask import  render_template, flash, redirect, url_for, request, abort
 from bop import app, db, bcrypt
-from bop.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from bop.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from bop.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [
-    {
-        'author': 'Jakub Blazewicz',
-        'title': 'Blog',
-        'content': 'frist post content',
-        'date_posted': 'November 1st, 2019'
-     },
-    {
-        'author': 'Aleksander Blazewicz',
-        'title': 'Bloger',
-        'content': 'second post content',
-        'date_posted': 'November 31st, 2019'
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
 
 @app.route("/contact")
 def contact():
@@ -41,6 +27,7 @@ def resources():
 
 @app.route("/review")
 def review():
+    posts = Post.query.all()
     return render_template('review.html', title='Reviews', posts=posts)
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -109,3 +96,52 @@ def account():
     image_file = url_for('static', filename='img/profile_pic/' + current_user.image_file)
     return render_template('account.html', title='account', image_file=image_file, form=form)
 
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your review has been created', 'success')
+        return redirect(url_for('review'))
+    return render_template('create_review.html', title='New Review', form=form, legend='New Post')
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your review has been updated', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_review.html', title='Update Review', form=form, legend='Update Review')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your review has been deleted', 'success')
+    return redirect(url_for('review'))
